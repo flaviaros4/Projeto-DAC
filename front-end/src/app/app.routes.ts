8a3c56e362807e6a05732a7f7c2cc97de448800a
leonardoalberto733 <leonardoalberto733@gmail.com>
2026-03-09 11:31:42 -0300

    [add] adicionei a tela de Login

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path : '', redirectTo: '/autocadastro', pathMatch: 'full' },
     { path: 'autocadastro', loadComponent: () => import('./pages/autocadastro/autocadastro').then(m => m.Autocadastro) },
+    { path: 'login', loadComponent:  () => import('./pages/login/login').then(m => m.Login)},
 ];
