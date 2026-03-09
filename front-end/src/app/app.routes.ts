874b0e5b474e748bbc3dfba108889ee6f194bec0
flaviaros4 <flaviarosa8@gmail.com>
2026-03-09 07:58:45 -0300

    tela de autocadastro - html + css

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', redirectTo: '/autocadastro', pathMatch: 'full' },
+    { path: 'autocadastro', loadComponent: () => import('./pages/autocadastro/autocadastro').then(m => m.Autocadastro) },
+];
---
+++ front-end/src/app/pages/autocadastro/autocadastro.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autocadastro',
+  imports: [],
+  templateUrl: './autocadastro.html',
+  styleUrl: './autocadastro.css',
+})
+export class Autocadastro {}
