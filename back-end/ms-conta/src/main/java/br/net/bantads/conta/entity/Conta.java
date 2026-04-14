a82bb0124a2ae4f0a207072adc1bdf91126637e4
leonardoalberto733 <leonardoalberto733@gmail.com>
2026-04-14 09:47:03 -0300

    adicionei a entidade de Conta

---
+++ back-end/ms-conta/src/main/java/br/net/bantads/conta/entity/Conta.java
@@ -0,0 +1,83 @@
+package br.net.bantads.conta.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Document (collection = "contas")
+
+public class Conta {
+    @Id
+    private String id;
+
+    private String cliente;
+
+    private String numero;
+
+    private BigDecimal saldo;
+
+    private BigDecimal limite;
+
+    private int gerente;
+    
+    private LocalDate criacao;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public BigDecimal getLimite() {
+        return limite;
+    }
+
+    public void setLimite(BigDecimal limite) {
+        this.limite = limite;
+    }
+
+    public int getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(int gerente) {
+        this.gerente = gerente;
+    }
+
+    public LocalDate getCriacao() {
+        return criacao;
+    }
+
+    public void setCriacao(LocalDate criacao) {
+        this.criacao = criacao;
+    }
+}
+
+
