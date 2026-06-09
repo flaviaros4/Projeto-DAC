93e3a92c4855c5bbb2a3f7ae248d2780ff74a076
leonardoalberto733 <leonardoalberto733@gmail.com>
2026-06-09 20:47:33 -0300

    adicionei dto de depositarResponse

---
+++ back-end/ms-conta/src/main/java/br/net/bantads/conta/dto/DepositarResponse.java
@@ -0,0 +1,70 @@
+package br.net.bantads.conta.dto;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import br.net.bantads.conta.entity.Tipo;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import br.net.bantads.conta.entity.Tipo;
+
+public class DepositarResponse {
+    public LocalDateTime data;
+    public Tipo tipo;
+    public String origem;
+    public String destino;
+    public BigDecimal valor;
+
+    public DepositarResponse(){
+
+    }
+
+    public DepositarResponse(LocalDateTime data, Tipo tipo, String origem, String destino, BigDecimal valor) {
+        this.data = data;
+        this.tipo = tipo;
+        this.origem = origem;
+        this.destino = destino;
+        this.valor = valor;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+    public String getDestino() {
+        return destino;
+    }
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
